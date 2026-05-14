44dfe27bb62ae4b4cd46eef3fe43cc89a2d6947b
jollyroger182 <hello@jollyy.dev>
2026-05-14 16:38:09 +0800

    initial commit

---
+++ scripts/build.ts
@@ -0,0 +1,15 @@
+await Promise.all([
+	Bun.$`bun check`,
+	Bun.build({
+		entrypoints: ['./index.ts'],
+		outdir: './dist/esm',
+		format: 'esm',
+		splitting: true,
+	}),
+	Bun.build({
+		entrypoints: ['./index.ts'],
+		outdir: './dist/cjs',
+		format: 'cjs',
+		splitting: true,
+	}),
+])
